a748b318bef2a49ef6ee14b9592da84ef8f16f1f
matir8 <martin.markov28@gmail.com>
2016-08-04 15:16:50 +0300

    added circles functionality

--- script.js
+++ script.js
@@ -5,32 +5,56 @@ var $wrapper = $('.wrapper'),
     ctx = canvas.getContext('2d'),
     color = 'black',
     rectSelect = false,
+    circleSelect = false,
+    x1,
+    y1,
+    x2,
+    y2,
     mouseDown = false;
-
-$tools.on('click', '#rectangle', function (ev) {
-    rectSelect = true;
-});
 
 function setColor(id) {
     color = id;
 }
 
+$tools.on('click', '#rectangle', function (ev) {
+    rectSelect = true;
+    circleSelect = false;
+});
+
+$tools.on('click', '#circle', function (ev) {
+    rectSelect = false;
+    circleSelect = true;
+});
+
 $(canvas).on('mousedown', function (ev) {
     mouseDown = true;
-    var startX = ev.pageX - findPos(this).x,
-        startY = ev.pageY - findPos(this).y;
-    if (rectSelect) {
-        $(canvas).on('mousemove', function (event) {
-            if (mouseDown) {
-                var endX = event.pageX - findPos(this).x,
-                    endY = event.pageY - findPos(this).y,
-                    width = endX - startX,
-                    height = endY - startY;
-                drawRect(startX, startY, width, height, color);
-            }
-        });
-    }
+    var rect = canvas.getBoundingClientRect();
+    x1 = ev.clientX - rect.left;
+    y1 = ev.clientY - rect.top;
+});
 
+$(canvas).on('mousemove', function (event) {
+    if (mouseDown) {
+        if (rectSelect) {
+            var rect = canvas.getBoundingClientRect(),
+                endX = event.pageX - rect.left,
+                endY = event.pageY - rect.top,
+                width = endX - x1,
+                height = endY - y1;
+            drawRect(x1, y1, width, height, color);
+        }
+        if (circleSelect) {
+            var rect = canvas.getBoundingClientRect(),
+                x2 = event.clientX - rect.left,
+                y2 = event.clientY - rect.top;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawEllipse(x1, y1, x2, y2, color);
+
+            ctx.strokeStyle = 'rgba(255, 0, 0, 0)';
+            ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        }
+    }
 });
 
 $(canvas).on('mouseup', function (ev) {
@@ -48,15 +72,25 @@ function drawRect(x, y, width, height, color) {
     ctx.strokeRect(x, y, width, height);
 }
 
-function findPos(obj) {
-    var curleft = 0, curtop = 0;
-    if (obj.offsetParent) {
-        do {
-            curleft += obj.offsetLeft;
-            curtop += obj.offsetTop;
-        } while (obj = obj.offsetParent);
-        return { x: curleft, y: curtop };
-    }
-    return undefined;
-}
+function drawEllipse(x1, y1, x2, y2, color) {
+    var radiusX = (x2 - x1) * 0.5,
+        radiusY = (y2 - y1) * 0.5,
+        centerX = x1 + radiusX,
+        centerY = y1 + radiusY,
+        step = 0.01,
+        a = step,
+        pi2 = Math.PI * 2 - step;
 
+    ctx.beginPath();
+    ctx.moveTo(centerX + radiusX * Math.cos(0),
+        centerY + radiusY * Math.sin(0));
+
+    for (; a < pi2; a += step) {
+        ctx.lineTo(centerX + radiusX * Math.cos(a),
+            centerY + radiusY * Math.sin(a));
+    }
+
+    ctx.closePath();
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
