68765e7ef94895aced00a22294cd5e5675ea0436
matir8 <martin.markov28@gmail.com>
2016-08-10 18:27:59 +0300

    fixed some things

--- script.js
+++ script.js
@@ -257,3 +257,15 @@ function changeCursirIcon(hasIcon, className) {
         $(tempcanvas).addClass(className);
     }
 }
+
+
+$('cropImageLink').on('click', function (ev) { 
+    var link = $(this);
+    link.attr('download', "MyCanvas.png");
+    link.attr('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+ });
+
+/*var link = document.getElementById('cropImageLink');
+link.setAttribute('download', 'MintyPaper.png');
+link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+link.click();*/
